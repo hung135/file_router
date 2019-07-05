f0441a33284c34283803318ed66301f40535d5dc
redspart <cdecker.developer@gmail.com>
2019-07-05 10:02:16 -0400

    Added verbose CLI option that will print and log both warnings and errors. Made it default print errors. Added sys.exit(1) on error not warning.

--- scripts/logic/file_history.py
+++ scripts/logic/file_history.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class FileHistory():
     @classmethod
-    def file_information(self, f, reg=None):
+    def file_information(self, logger, f, reg=None):
         try:
             _extract = None
             _md5 = hashlib.md5(open(f, "rb").read()).hexdigest()
@@ -13,19 +13,21 @@ class FileHistory():
             _date = datetime.fromtimestamp(os.stat(f).st_mtime)
 
             if reg is not None:
-                _extract = self.file_path_extract(f, reg)
+                _extract = self.file_path_extract(f, reg, logger)
 
             return (_md5, _size, _date, _extract)
         except Exception as e:
-            print("Something went wrong \n %s" % (e))
+            if logger is not None:
+                logger.warning("Something went wrong trying to get file information for: {0}, e: {1}".format(f, e))
             return (None, None, None, None)
 
     @staticmethod
-    def file_path_extract(f, reg):
+    def file_path_extract(f, reg, logger):
         try:
             reg = re.compile(reg)
             found = re.findall(reg, f)
             return None if len(found) == 0 else found[-1]
         except re.error:
-            print("invalid regex, skipping")
+            if logger is not None:
+                logger.warning("Invalid regex {0}, skipping".format(reg))
             return None
