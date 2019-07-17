e680d5c492bf30962b746d45b47ebf683fbdb793
redspart <cdecker.developer@gmail.com>
2019-07-17 14:02:19 +0000

    Updated github-manager.py to include adding assets on creation of a release. adding assets to a cuurrent release.

--- scripts/version.py
+++ scripts/version.py
@@ -1,2 +1,7 @@
-version_dict = {"NB": "This needs to be injected by build process"
+version_dict = {"git_hash":"""
+fc2ff0c6ae267602a264c972a919cfa77ef52f28
+""",
+"build_time":"""
+Wed Jul 17 12:53:45 UTC 2019
+"""
 }
